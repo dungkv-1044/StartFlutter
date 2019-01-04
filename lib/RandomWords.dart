10025b7ac92274b9bbc40ccdbb5ec5269195cefc
khuat.van.dung <khuat.van.dung@Linux.local>
2019-01-04 15:20:45 +0700

    Init app

---
+++ lib/RandomWords.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'RandomWordsState.dart';
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
